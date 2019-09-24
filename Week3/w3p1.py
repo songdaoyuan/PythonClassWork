56f09bc10df6dbc96461fe0a3fc30c3e36151810
宋道源 <32031697+songdaoyuan@users.noreply.github.com>
2019-09-24 16:59:05 +0800

    Create w3p1.py

---
+++ Week3/w3p1.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+# code by 宋道源 @ 2019.9.24, Test on Python 3.74 64-bit
+a, b, c = map(str, input().split())
+print(f"The value which was input from your keyboard is: {a}, {b}, {c}")
